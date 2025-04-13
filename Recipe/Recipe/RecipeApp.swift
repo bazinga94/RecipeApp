ab32de316a5ab694bedb1308bc764286b6bc78c9
Jongho Lee <bazinga940@gmail.com>
2025-04-13 14:04:45 -0400

    Create xcode project

---
+++ Recipe/Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Jongho Lee on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
